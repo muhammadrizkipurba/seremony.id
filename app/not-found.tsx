6348e349205a32ffd4ebc7cf705110d41d3a60be
Mhd Rizki Purba <muhammadrizkipurba7@gmail.com>
2025-11-18 13:33:15 +0700

    [fix] not found update component name

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,15 @@
+"use client"
 import React from 'react';
 import Link from "next/link";
+import dynamic from 'next/dynamic';
 import { HiArrowLeft } from "react-icons/hi2";
 import NotFoundLottie from '@/app/404-not-found.json';
 
 import MainLayout from "@/components/layout/MainLayout";
-import LottiePlayer from '@/components/LottiePlayer';
+
+const LottiePlayer = dynamic(() => import('../components/LottiePlayer'), {
+  ssr: false,
+});
 
 export default function NotFound() {
   return (
@@ -23,7 +28,7 @@ export default function NotFound() {
               </Link>
             </div>
           </div>
-          <LottiePlayer 
+          <LottiePlayer
             src={NotFoundLottie}
             autoplay
             loop
